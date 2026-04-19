ee0f3f4609acd7d91498d508add3d3a7975da2c7
hrut76-ship-it <hrut76@gmail.com>
2026-04-19 14:52:47 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+# Margaret's Spanish/English List
+rules = {
+    "RED": ["oat", "avena", "potato", "patata", "wheat", "trigo", "cashew", "anacardo", "almond", "almendra", "yuca", "spelt", "espelta", "plum", "ciruela", "cow milk", "leche de vaca", "walnut", "nuez", "pistacho"],
+    "ORANGE": ["parsley", "perejil", "peach", "melocoton", "spinach", "espinaca", "tomato", "tomate", "mussel", "mejillon", "soja", "centeno", "fresa", "sandia", "coco", "mango", "apio", "remolacha"],
+    "YELLOW": ["banana", "platano", "aguacate", "miel", "frambuesa", "datil", "mostaza", "azucar", "chufa", "lino"]
+}
